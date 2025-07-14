32bea8f4a6368fd3b05e1e021574c9f17db60a95
Elavarasi28 <rtr.elavarasimurugappillai@gmail.com>
2025-07-15 00:17:45 +0530

    Initial commit

--- learning/src/App.tsx
+++ learning/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sidebar from "./sidebar";
 import Home from "./home";
-
+ssh-keygen -t ed25519 -c 
 
 function App() {
   return (
